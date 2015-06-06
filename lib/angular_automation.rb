302c846c41d0238a971b3c06c28f39f44eeca3e3
bootstraponline <code@bootstraponline.com>
2015-06-06 17:14:39 -0400

    Update requires

--- lib/angular_automation.rb
+++ lib/angular_automation.rb
@@ -1,14 +1,16 @@
 require 'rubygems'
-require 'webdriver_utils'
+
 require 'rspec_flake'
 require 'yarjuf' # rspec_junit
 require 'angular_page_object'
+require 'angular_webdriver' # requires selenium-webdriver and watir-webdriver
 require 'page_object_stubs'
 require 'sauce' # sauce_ruby
 require 'trace_files'
 require 'pry'
+require 'sauce_platforms'
+require 'webdriver_utils'
 require 'rspec'
-require 'angular_webdriver' # requires selenium-webdriver and watir-webdriver
 
 # internal
 require_relative 'angular_automation/version'
