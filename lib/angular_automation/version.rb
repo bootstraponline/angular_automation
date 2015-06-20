a9123d6b17b0019482e070a71a8f8b7c0e36c113
bootstraponline <code@bootstraponline.com>
2015-06-20 11:51:43 -0400

    Release 1.0.6

--- lib/angular_automation/version.rb
+++ lib/angular_automation/version.rb
@@ -1,4 +1,4 @@
 module AngularAutomation
-  VERSION = '1.0.5' unless defined? ::AngularAutomation::VERSION
+  VERSION = '1.0.6' unless defined? ::AngularAutomation::VERSION
   DATE    = '2015-06-20' unless defined? ::AngularAutomation::DATE
 end
