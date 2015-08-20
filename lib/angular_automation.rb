b83424ac89e3357afeb80b81805641f3dc9ae2da
bootstraponline <code@bootstraponline.com>
2015-08-20 12:10:31 -0400

    Don't require sauce by default

--- lib/angular_automation.rb
+++ lib/angular_automation.rb
@@ -5,12 +5,18 @@ require 'rspec_junit'
 require 'page_object'
 require 'angular_webdriver' # requires selenium-webdriver and watir-webdriver
 require 'page_object_stubs'
-require 'sauce' # sauce_ruby
 require 'trace_files'
 require 'pry'
 require 'sauce_platforms'
 require 'webdriver_utils'
 require 'rspec'
+
+# sauce_ruby is **not** required by default so that angular_automation
+# can easily work with other cloud providers. also the sauce gem
+# inserts rspec hooks that are sauce specific.
+#
+# require 'sauce'
+
 
 # internal
 require_relative 'angular_automation/version'
